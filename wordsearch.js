1ef3616636df0c0f3fd80c28e8b6ddc53e7eb982
NKadish <noahkadish@hotmail.com>
2020-09-23 21:59:18 +0000

    functional word search that looks horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
+const transpose = function(matrix) {
+    // Put your solution here
+    for (let i = 0; i < matrix.length; i++) {
+      for ( let j = 0; j < i; j++) {
+        let temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }
+    return matrix;
+  };
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+    let verticalJoin = transpose(letters);
+    console.log(horizontalJoin);
+    verticalJoin = verticalJoin.map(ls => ls.join(''));
+    console.log(verticalJoin);
+    let resultHolder = false;
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            resultHolder = true;
+            return resultHolder;
+        }
+    } 
+    for (const i of verticalJoin) {
+        if (i.includes(word)) {
+            resultHolder = true;
+            return resultHolder;
+        }
+    } 
+    return resultHolder;
 }
-
 module.exports = wordSearch
+
+// Pair programmed by Noah and Jacqueline! 
